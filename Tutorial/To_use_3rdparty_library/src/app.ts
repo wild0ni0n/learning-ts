5ac75d18cee7f4360d48b16e06c646ebd94f73ee
wild0ni0n <yuisuwako@gmail.com>
2022-12-20 14:35:23 +0900

    add 3rd library

--- Tutorial/To_use_3rdparty_library/src/app.ts
+++ Tutorial/To_use_3rdparty_library/src/app.ts
@@ -1,3 +1,28 @@
-import _ from 'lodash';
+import "reflect-metadata";
+import { plainToClass } from 'class-transformer';
+import { validate } from "class-validator";
+import { Product } from "./product.model";
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [{ title: '商品1', price: 100 }, { title: '商品2', price: 200 }];
+
+const newProd = new Product('', -100);
+validate(newProd).then(errors => {
+    if (errors.length > 0) {
+        console.log("validation error");
+        console.log(errors);
+    } else {
+        console.log(newProd.getInformation());
+    }
+});
+// const p1 = new Product('商品1', 100);
+
+// const loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price);
+// })
+
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
